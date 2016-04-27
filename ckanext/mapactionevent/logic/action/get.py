ea4ea98a3201b83270fa24059917ff72276eff91
Martin Burchell <martinb@aptivate.org>
2016-04-27 10:51:36 +0100

    Fixed pagination for events ordered by date

--- ckanext/mapactionevent/logic/action/get.py
+++ ckanext/mapactionevent/logic/action/get.py
@@ -4,23 +4,36 @@ import ckan.plugins.toolkit as toolkit
 def event_list(context, data_dict):
     sort = data_dict.get('sort') or 'created desc'
 
-    all_fields = toolkit.asbool(data_dict.get('all_fields', None))
-
-    if sort == 'created desc':
-        data_dict.update(sort='name',
-                         all_fields=True)
-
     data_dict['type'] = 'event'
+
+    if sort != 'created desc':
+        return toolkit.get_action('group_list')(
+            context,
+            data_dict=data_dict)
+
+    return _list_by_created_desc(context, data_dict)
+
+
+def _list_by_created_desc(context, data_dict):
+    all_fields = toolkit.asbool(data_dict.get('all_fields', None))
+    offset = data_dict.get('offset', None)
+    limit = data_dict.get('limit', None)
+    if limit:
+        end = offset+limit
+    else:
+        end = None
+
+    data_dict.update(sort='name',
+                     all_fields=True,
+                     limit=0,
+                     offset=0)
 
     groups = toolkit.get_action('group_list')(
         context,
         data_dict=data_dict)
 
-    if sort != 'created desc':
-        return groups
-
     sorted_groups = sorted(groups, key=lambda group: group['created'],
-                           reverse=True)
+                           reverse=True)[offset:end]
 
     if all_fields:
         return sorted_groups
--- ckanext/mapactionevent/tests/logic/action/test_get.py
+++ ckanext/mapactionevent/tests/logic/action/test_get.py
@@ -14,37 +14,92 @@ class TestEventList(custom_helpers.FunctionalTestBaseClass):
         super(TestEventList, self).setup()
         self.user = factories.User()
 
-        self.albania_2010 = helpers.call_action(
-            'event_create',
-            context={'user': self.user['name']},
-            title='Albania Floods, January 2010',
-            created=datetime(2010, 1, 1),
-            users=[{'name': self.user, 'capacity': 'admin'}])
-
-        self.benin_2009 = helpers.call_action(
-            'event_create',
-            context={'user': self.user['name']},
-            title='Benin Floods, July 2009',
-            created=datetime(2009, 7, 1),
-            users=[{'name': self.user, 'capacity': 'admin'}])
-
-        self.cape_verde_2014 = helpers.call_action(
-            'event_create',
-            context={'user': self.user['name']},
-            title='Cape Verde, December 2014',
-            created=datetime(2014, 12, 1),
-            users=[{'name': self.user, 'capacity': 'admin'}])
-
     def test_event_names_listed_by_created_descending(self):
+        albania_2010 = self._get_new_event(
+            'Albania Floods, January 2010',
+            datetime(2010, 1, 1))
+
+        benin_2009 = self._get_new_event(
+            'Benin Floods, July 2009',
+            datetime(2009, 7, 1))
+
+        cape_verde_2014 = self._get_new_event(
+            'Cape Verde, December 2014',
+            datetime(2014, 12, 1))
+
         event_names = helpers.call_action(
             'event_list')
 
         assert_equals(event_names,
-                      [self.cape_verde_2014['name'],
-                       self.albania_2010['name'],
-                       self.benin_2009['name']])
+                      [cape_verde_2014['name'],
+                       albania_2010['name'],
+                       benin_2009['name']])
+
+    def test_event_names_listed_by_created_descending_pagination(self):
+        albania_2010 = self._get_new_event(
+            'Albania Floods, January 2010',
+            datetime(2010, 1, 1))
+
+        benin_2009 = self._get_new_event(
+            'Benin Floods, July 2009',
+            datetime(2009, 7, 1))
+
+        cape_verde_2014 = self._get_new_event(
+            'Cape Verde, December 2014',
+            datetime(2014, 12, 1))
+
+        dominica_2015 = self._get_new_event(
+            'Dominica - TS Erika, September 2015',
+            datetime(2015, 9, 1))
+
+        east_africa_2006 = self._get_new_event(
+            'East Africa Seismic Risk, March 2006',
+            datetime(2006, 3, 1))
+
+        fiji_2016 = self._get_new_event(
+            'Fiji - Cyclone Winston, February 2016 ',
+            datetime(2016, 2, 1))
+
+        event_names = helpers.call_action(
+            'event_list',
+            limit=2,
+            offset=0)
+
+        assert_equals(event_names,
+                      [fiji_2016['name'],
+                       dominica_2015['name']])
+
+        event_names = helpers.call_action(
+            'event_list',
+            limit=2,
+            offset=2)
+
+        assert_equals(event_names,
+                      [cape_verde_2014['name'],
+                       albania_2010['name']])
+
+        event_names = helpers.call_action(
+            'event_list',
+            limit=2,
+            offset=4)
+
+        assert_equals(event_names,
+                      [benin_2009['name'],
+                       east_africa_2006['name']])
 
     def test_events_listed_by_created_descending(self):
+        albania_2010 = self._get_new_event(
+            'Albania Floods, January 2010',
+            datetime(2010, 1, 1))
+
+        benin_2009 = self._get_new_event(
+            'Benin Floods, July 2009',
+            datetime(2009, 7, 1))
+
+        cape_verde_2014 = self._get_new_event(
+            'Cape Verde, December 2014',
+            datetime(2014, 12, 1))
+
         events = helpers.call_action(
             'event_list',
             all_fields=True)
@@ -52,16 +107,36 @@ class TestEventList(custom_helpers.FunctionalTestBaseClass):
         event_names = [e['name'] for e in events]
 
         assert_equals(event_names,
-                      [self.cape_verde_2014['name'],
-                       self.albania_2010['name'],
-                       self.benin_2009['name']])
+                      [cape_verde_2014['name'],
+                       albania_2010['name'],
+                       benin_2009['name']])
 
     def test_event_names_listed_by_name_ascending(self):
+        albania_2010 = self._get_new_event(
+            'Albania Floods, January 2010',
+            datetime(2010, 1, 1))
+
+        benin_2009 = self._get_new_event(
+            'Benin Floods, July 2009',
+            datetime(2009, 7, 1))
+
+        cape_verde_2014 = self._get_new_event(
+            'Cape Verde, December 2014',
+            datetime(2014, 12, 1))
+
         event_names = helpers.call_action(
             'event_list',
             sort='name asc')
 
         assert_equals(event_names,
-                      [self.albania_2010['name'],
-                       self.benin_2009['name'],
-                       self.cape_verde_2014['name']])
+                      [albania_2010['name'],
+                       benin_2009['name'],
+                       cape_verde_2014['name']])
+
+    def _get_new_event(self, title, date):
+        return helpers.call_action(
+            'event_create',
+            context={'user': self.user['name']},
+            title=title,
+            created=date,
+            users=[{'name': self.user, 'capacity': 'admin'}])
